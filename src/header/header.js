7d96a42f5642c036e32fac409eb84e4f78ec3788
Popov Volodymyr <marelyru200@gmail.com>
2022-04-03 14:56:24 +0300

    Connect modules css

--- src/header/header.js
+++ src/header/header.js
@@ -1,50 +1,50 @@
 import React, { Compomemt } from "react";
-import "./header.include.css";
+import header from "./header.module.css";
 
 const Header = () => {
   return (
     <header>
-      <div className="menuBlock">
-        <div className="headerTitle">April 2022</div>
-        <div className="headerMenu">
-          <div className="imgMenu"></div>
+      <div className={header.menuBlock}>
+        <div className={header.headerTitle}>April 2022</div>
+        <div className={header.headerMenu}>
+          <div className={header.imgMenu}></div>
         </div>
       </div>
-      <div className="dayBlock">
-        <div className="dayData">
-          <div className="number">19</div>
-          <div className="day">Su</div>
-          <div className="noactiveToDo"></div>
+      <div className={header.dayBlock}>
+        <div className={header.dayData}>
+          <div className={header.number}>19</div>
+          <div className={header.day}>Su</div>
+          <div className={header.noactiveToDo}></div>
         </div>
-        <div className="dayData">
-          <div className="number">20</div>
-          <div className="day">Mo</div>
-          <div className="activeToDo"></div>
+        <div className={header.dayData}>
+          <div className={header.number}>20</div>
+          <div className={header.day}>Mo</div>
+          <div className={header.activeToDo}></div>
         </div>
-        <div className="dayData">
-          <div className="number">21</div>
-          <div className="day">Tu</div>
-          <div className="noactiveToDo"></div>
+        <div className={header.dayData}>
+          <div className={header.number}>21</div>
+          <div className={header.day}>Tu</div>
+          <div className={header.noactiveToDo}></div>
         </div>
-        <div className="dayData active">
-          <div className="number">22</div>
-          <div className="day">We</div>
-          <div className="noactiveToDo"></div>
+        <div className={header.dayDataActive}>
+          <div className={header.number}>22</div>
+          <div className={header.day}>We</div>
+          <div className={header.noactiveToDo}></div>
         </div>
-        <div className="dayData">
-          <div className="number">23</div>
-          <div className="day">Su</div>
-          <div className="activeToDo"></div>
+        <div className={header.dayData}>
+          <div className={header.number}>23</div>
+          <div className={header.day}>Su</div>
+          <div className={header.activeToDo}></div>
         </div>
-        <div className="dayData">
-          <div className="number">24</div>
-          <div className="day">Mo</div>
-          <div className="activeToDo"></div>
+        <div className={header.dayData}>
+          <div className={header.number}>24</div>
+          <div className={header.day}>Mo</div>
+          <div className={header.activeToDo}></div>
         </div>
-        <div className="dayData">
-          <div className="number">25</div>
-          <div className="day">Tu</div>
-          <div className="noactiveToDo"></div>
+        <div className={header.dayData}>
+          <div className={header.number}>25</div>
+          <div className={header.day}>Tu</div>
+          <div className={header.noactiveToDo}></div>
         </div>
       </div>
     </header>
